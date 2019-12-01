1262a38bbf85b54b41d1c97a7836ed6961f5f955
KarimAmrouche <m.amr@outlook.fr>
2019-12-01 01:48:21 +0100

    initial import

---
+++ td00/src/main/java/fr/dauphine/ja/amrouchekarim/td00/PrimeCollection.java
@@ -0,0 +1,48 @@
+package fr.dauphine.ja.amrouchekarim.td00;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class PrimeCollection {
+	protected ArrayList<Integer> numbers;
+
+	public PrimeCollection() {
+		this.numbers = new ArrayList<Integer>();
+	}
+
+	public void initRandom(int n, int m) {
+		Random random = new Random();
+		for (int i = 0; i < n; i++) {
+			this.numbers.add(random.nextInt(m));
+		}
+	}
+
+	public boolean isPrime(Integer p) {
+		boolean r = true;
+		for (float x = 2; x < Math.sqrt(p); x++)
+			if (p % x == 0)
+				r = false;
+		return r;
+	}
+
+	public void printPrimes() {
+		for (Integer integer : numbers) {
+			if (isPrime(integer))
+				System.out.println(integer);
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		PrimeCollection p = new PrimeCollection();
+		p.initRandom(100, 1000);
+		p.printPrimes();
+
+	}
+
+}
