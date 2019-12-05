fbfe76cc54291f1a87068fa9b0606f385aa4ed3a
KarimAmrouche <m.amr@outlook.fr>
2019-12-05 14:48:47 +0100

    fusion modified

--- generics/src/main/java/fr/dauphine/ja/amrouchekarim/generics/Main.java
+++ generics/src/main/java/fr/dauphine/ja/amrouchekarim/generics/Main.java
@@ -2,6 +2,9 @@ package fr.dauphine.ja.amrouchekarim.generics;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -10,19 +13,26 @@ import java.util.List;
  * @param <T>
  *
  */
-public class Main<T> {
+public class Main {
 
 	public static void main(String[] args) {
 
-		List l = Arrays.asList("colonel", "reyel");
-		System.out.println(listLength(l));
+		List<String> l1= Arrays.asList("C", "rc");
+		List<StringBuilder> l2= Arrays.asList(new StringBuilder("a ma"), new StringBuilder("he!"));
+		List<? extends CharSequence> r1=fusion(l1,l2);
+		List<?> r2=fusion(l1,l2);
+		List<Integer> l3 = Arrays.asList(1,2);
+		List<Integer> l4 = Arrays.asList(10,20);
+		List<Integer> r3 = fusion(l3,l4);
+		List<?> r4 = fusion(l1,l3);
+
 	}
 
 	private static <T extends Comparable<T>> T myMax(T x, T... y) {
 		T max = x;
 		for (int i = 0; i < y.length; i++) {
 			if (max.compareTo(y[i]) < 0)
-				max = y[i];
+				max = y[i];s
 		}
 		return max;
 	}
@@ -44,12 +54,27 @@ public class Main<T> {
 //		}
 //		return length;
 //	}
-	
+
 	public static ArrayList listLength(List<? extends CharSequence> list) {
 		ArrayList length = new ArrayList();
 		for (int i = 0; i < list.size(); i++) {
 			length.add(list.get(i).length());
 		}
 		return length;
+	}
+
+	public static <T> List<T> fusion(List<? extends T> liste1, List<? extends T> liste2 ){
+		List<T>list = new ArrayList<T>();
+		if(liste1.size()!=liste2.size() || liste1.isEmpty())
+			return Collections.<T>emptyList();
+		else {
+			Iterator<? extends T> it1 = liste1.iterator();
+			Iterator<? extends T> it2 = liste1.iterator();
+			while (it2.hasNext() || it1.hasNext()) {
+				list.add(it1.next());
+				list.add(it2.next());
+				}
+			}
+		return list;
 	}
 }
